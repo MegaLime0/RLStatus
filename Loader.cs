d1d85d9525ee9a9a5a9a6baabdb18ad6e9163b0a
MegaLime <72345639+MegaLime0@users.noreply.github.com>
2024-03-15 00:17:39 +0400

    Add String Trimming To Loader
    The loader class now trims every string in case the token files contain
    a newline character

--- Loader.cs
+++ Loader.cs
@@ -4,11 +4,11 @@ public static class Loader
 {
     public static string DiscordToken(string path = ".discord_token")
     {
-        return File.ReadAllText(path);
+        return File.ReadAllText(path).Trim();
     }
 
     public static string SteamWebAPI(string path = ".steam_api")
     {
-        return File.ReadAllText(path);
+        return File.ReadAllText(path).Trim();
     }
 }
