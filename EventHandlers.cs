2aaddfc1ae64dac808d3b9e74197ffb2a92914fd
MegaLime <72345639+MegaLime0@users.noreply.github.com>
2024-03-15 12:39:55 +0400

    Reformat EventHandlers Slightly

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -8,14 +8,14 @@ static public class EventHandlers
     static public Task OnReady(DiscordClient client, ReadyEventArgs args)
     {
         Console.WriteLine(
-                $"Client {client.CurrentUser.Username}#{client.CurrentUser.Discriminator}" 
+                $"Client {client.CurrentUser.Username}#{client.CurrentUser.Discriminator}"
                 + " Successfully Connected");
         return Task.CompletedTask;
     }
 
     static public async Task OnMessage(DiscordClient client, MessageCreateEventArgs args)
     {
-        if (args.Message.Content == "\\summon tnt") 
+        if (args.Message.Content == "\\summon tnt")
         {
             await args.Channel.SendMessageAsync("T-T");
         }
