039e8b7a02445f43783a9b43931e8b078b41df12
MegaLime <72345639+MegaLime0@users.noreply.github.com>
2024-03-15 22:04:30 +0400

    Add Mode.cs Constructor

--- Mode.cs
+++ Mode.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace RLStatus;
 
 public class Mode
@@ -5,4 +7,16 @@ public class Mode
     public uint MMR { get; private set; }
     public uint Division { get; private set; }
     public Ranks Rank { get; private set; }
+
+    public Mode(JsonElement stats)
+    {
+        stats = stats.GetProperty("stats");
+        MMR = stats.GetProperty("rating")
+            .GetProperty("value")
+            .GetUInt32();
+
+        Rank = (Ranks) ((stats.GetProperty("tier").GetProperty("value").GetInt16() - 1) / 3);
+
+        Division = stats.GetProperty("division").GetProperty("value").GetUInt16() + (uint) 1;
+    }
 }
