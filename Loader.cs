e5448970e663f5a827d6224aa400e2679b78335f
MegaLime0 <72345639+MegaLime0@users.noreply.github.com>
2024-03-23 17:42:38 +0400

    Fix Embarrasing Bug

--- Loader.cs
+++ Loader.cs
@@ -2,12 +2,12 @@ namespace RLStatus;
 
 public static class Loader
 {
-    public static string DiscordToken(string path = ".discord_token")
+    public static string DiscordToken(string path = "tokens.txt")
     {
         return File.ReadAllLines(path)[0].Trim();
     }
 
-    public static string SteamWebAPI(string path = ".steam_api")
+    public static string SteamWebAPI(string path = "tokens.txt")
     {
         return File.ReadAllLines(path)[1].Trim();
     }
