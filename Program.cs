806ecc81f5b4925ffd44db6fbb52f44d9ca49414
MegaLime0 <72345639+MegaLime0@users.noreply.github.com>
2024-03-25 16:09:43 +0400

    Refactor Program.cs

--- Program.cs
+++ Program.cs
@@ -7,26 +7,31 @@ public class Program
 {
     static async Task Main()
     {
+        await StartBot();
+        await Task.Delay(-1);
+
+    }
+
+    static async Task StartBot()
+    {
         string _token = Loader.DiscordToken();
         DiscordConfiguration conf = new() 
         {
             Token = _token,
-            TokenType = TokenType.Bot,
-            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+                  TokenType = TokenType.Bot,
+                  Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
         };
-       
+
         DiscordClient client = new(conf);
 
         var slashCMDS = client.UseSlashCommands();
 
-        slashCMDS.RegisterCommands<SlashCommands>(940320639498223677);
+        slashCMDS.RegisterCommands<SlashCommands>();
+        SlashCommands.StoreClient(client);
 
         await client.ConnectAsync();
 
-
         client.Ready += EventHandlers.OnReady;
         client.MessageCreated += EventHandlers.OnMessage;
-
-        await Task.Delay(-1);
     }
 }
