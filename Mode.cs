c81eff77b0b10306ae3bc157dce748bd0da8ea96
MegaLime0 <72345639+MegaLime0@users.noreply.github.com>
2024-03-25 16:08:52 +0400

    Make Mode.cs Integers Signed

--- Mode.cs
+++ Mode.cs
@@ -4,12 +4,12 @@ namespace RLStatus;
 
 public class Mode
 {
-    public uint MMR { get; private set; }
-    public uint Division { get; private set; }
+    public int MMR { get; private set; }
+    public int Division { get; private set; }
     public Ranks Rank { get; private set; }
     public Playlists Playlist { get; set; }
 
-    public Mode(uint _mmr, uint _div, Ranks _rank, Playlists _plst)
+    public Mode(int _mmr, int _div, Ranks _rank, Playlists _plst)
     {
         MMR = _mmr;
         Division = _div;
